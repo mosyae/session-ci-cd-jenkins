89b05e88eeb6319959ba9cae33d2c0d7af8416b0
mosyae <33257923+mosyae@users.noreply.github.com>
2018-01-24 15:03:31 +0200

    Update jenkins_slave_setup.sh

--- setup/jenkins_slave_setup.sh
+++ setup/jenkins_slave_setup.sh
@@ -7,3 +7,4 @@ apt-get install default-jdk -qq
 #docker must be installed on slave
 #local docker repositiry musy be installed on slave
 #local docker-compose must be installed on the slave
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
